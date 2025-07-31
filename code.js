b67dafbef90fae0d54fe3fc828d0cd6ccd19e83a
shahsarthak547 <112324539+shahsarthak547@users.noreply.github.com>
2025-07-31 12:02:30 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,50 @@
+const boxes = document.querySelectorAll(".box");
+const inputBox = document.getElementById("inputBox");
+const resetBtn = document.getElementById("reset");
+const msgContainer = document.querySelector(".msg-container");
+const msg = document.getElementById("msg");
+const submitBtn = document.getElementById("submit");
+const back = document.getElementById("backspace");
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessCount = 0;
+const maxGuess = 7;
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    inputBox.value += box.innerText;
+  });
+});
+submitBtn.addEventListener("click", () => {
+  const guess = parseInt(inputBox.value);
+  if(!guess || guess < 1 || guess > 100){
+    inputBox.value = "";
+    msg.innerText = "Please enter a number between 1 and 100";
+    msgContainer.classList.remove("hide");
+    return;
+  }
+  guessCount++;
+  if(guess === randomNumber){
+    msg.innerText = "🎉 YOU WON!";
+    msgContainer.classList.remove("hide");
+    inputBox.disabled = true;
+    submitBtn.disabled = true;
+  }
+  else {
+    msg.innerText = guess > randomNumber ? "Too high!" : "Too low!";
+    msgContainer.classList.remove("hide");
+  }
+  if(guessCount == maxGuess){
+    msg.innerText = `The answer was ${randomNumber}`;
+    inputBox.disabled = true;
+    submitBtn.disabled = true;
+  }
+});
+back.addEventListener("click", () => {
+  inputBox.value = inputBox.value.slice(0,-1);
+})
+resetBtn.addEventListener("click", () => {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  inputBox.value = "";
+  inputBox.disabled = false;
+  submitBtn.disabled = false;
+  msgContainer.classList.add("hide");
+});
